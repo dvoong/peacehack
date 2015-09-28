4b5571018e3490d944db88bfae3046fbeed7fe7a
David Voong <voong.david@gmail.com>
2015-09-28 13:21:09 +0100

    Add hugs not thugs presentation

--- peacehack/views.py
+++ peacehack/views.py
@@ -2,10 +2,6 @@ import requests
 from django.shortcuts import render
 
 def home(request):
-    try:
-        postcode = request.GET['postcode']
-    except KeyError:
-        postcode = None
     response = requests.get('https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2013-01')
     entries = response.json()[:]
     return render(request, 'home.html', {'entries': entries})
